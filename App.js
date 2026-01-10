405784fa1261f18f27aa4c52ed1a6f75bd221522
IsaacBarros <isaacbarros@alu.ufc.br>
2026-01-10 19:18:37 -0300

    deu bom caralhoww

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>ojkiaoao oio oio ioi o i oi o i o</Text>
+      <Text>ojsjsj</Text>
       <StatusBar style="auto" />
     </View>
   );
